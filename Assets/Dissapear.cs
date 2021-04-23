a0fa8028020bdb09c295eee1f2ade28af06eb191
bkocaman <berkkocaman@hotmail.com>
2021-04-23 19:23:53 +0300

    Coins and GameOver Scene Editted

---
+++ Assets/Dissapear.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissapear : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Droplatform()
+    {
+        rb2d.isKinematic = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player" && GameObject.Find("Player").GetComponent<Rigidbody2D>().velocity.y == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,30 +5,55 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
-	public Animator panelGameOverAnim;
-	public Text gameScore;
-	public Text menuScore;
+    public Animator panelGameOverAnim;
+    public Text gameScore;
+    public Text menuScore;
+    public float maxDistanceBeforeLose = 5f;
+    private float distance;
+    public Transform target;
+    public float startLimmit = 5f;
+    private Vector3 targetPosition;
+	
+	
 
     public void GameOver()
-	{
-		panelGameOverAnim.SetTrigger("Open");
-		GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().enabled = false;
-		menuScore.text = gameScore.text;
-		gameScore.gameObject.SetActive(false);
-	}
+    {
+        panelGameOverAnim.SetTrigger("Open");
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().enabled = false;
+        menuScore.text = gameScore.text;
+        gameScore.gameObject.SetActive(false);
+    }
 
-	public void PlayAgain()
-	{
-		UnityEngine.SceneManagement.SceneManager.LoadScene(0);
-	}
+    public void PlayAgain()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
 
-	void LateUpdate()
-	{
-		if (ScoreManager.coin == 2)
-		{
-			GameOver();
-		}
-	}
+    void LateUpdate()
+    {
+        if (ScoreManager.coin == 3)
+        {
+            GameOver();
+        }
+		
+        distance = target.position.y - transform.position.y;
 
-	
+        if (target.position.y < startLimmit)
+            return;
+		
+        if (distance < -maxDistanceBeforeLose)
+        {
+            GameOver();
+        }
+        else if (distance > 1)
+        {
+            targetPosition = new Vector3(0, target.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, distance*Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime);
+        }
+		
+    }
 }
